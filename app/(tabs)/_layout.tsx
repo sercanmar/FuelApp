3f43a32f8f32c92866647c421f1372e10faa4c47
sercanmar <sercanmar@alu.edu.gva.es>
2026-02-01 19:08:36 +0100

    Creación proyecto

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import "../global.css";
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
